a117d326f5b7ec7e1eb2e19a10cb7b0ead5094ec
Higan Higa <94578753+NguyenVanPhan07082000@users.noreply.github.com>
2022-12-21 16:47:40 +0700

    edit home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,11 +19,12 @@ import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
 import InboxIcon from "@mui/icons-material/MoveToInbox";
 import MailIcon from "@mui/icons-material/Mail";
-import { Badge, InputBase, Menu, MenuItem, Stack } from "@mui/material";
+import { Avatar, Badge, InputBase, Menu, MenuItem, Stack } from "@mui/material";
 import NotificationsIcon from "@mui/icons-material/Notifications";
 import { AccountCircle } from "@mui/icons-material";
 import SearchIcon from "@mui/icons-material/Search";
 import MoreIcon from "@mui/icons-material/MoreVert";
+import AvatarImage from "../public/avatar.jpg";
 
 const drawerWidth = 240;
 
@@ -253,14 +254,14 @@ export default function Home() {
               component="div"
               sx={{ display: { xs: "none", sm: "block" } }}
             >
-              MUI
+              BLUE START
             </Typography>
             <Search>
               <SearchIconWrapper>
                 <SearchIcon />
               </SearchIconWrapper>
               <StyledInputBase
-                placeholder="Search…"
+                placeholder="Tìm kiếm..."
                 inputProps={{ "aria-label": "search" }}
               />
             </Search>
@@ -293,7 +294,7 @@ export default function Home() {
                 onClick={handleProfileMenuOpen}
                 color="inherit"
               >
-                <AccountCircle />
+                <Avatar alt="Hình ảnh" src={AvatarImage.src} />
               </IconButton>
             </Box>
             <Box sx={{ display: { xs: "flex", md: "none" } }}>
@@ -363,36 +364,112 @@ export default function Home() {
         </Drawer>
         <Main open={open}>
           <DrawerHeader />
-          <Typography paragraph>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Rhoncus
-            dolor purus non enim praesent elementum facilisis leo vel. Risus at
-            ultrices mi tempus imperdiet. Semper risus in hendrerit gravida
-            rutrum quisque non tellus. Convallis convallis tellus id interdum
-            velit laoreet id donec ultrices. Odio morbi quis commodo odio aenean
-            sed adipiscing. Amet nisl suscipit adipiscing bibendum est ultricies
-            integer quis. Cursus euismod quis viverra nibh cras. Metus vulputate
-            eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo
-            quis imperdiet massa tincidunt. Cras tincidunt lobortis feugiat
-            vivamus at augue. At augue eget arcu dictum varius duis at
-            consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem
-            donec massa sapien faucibus et molestie ac.
-          </Typography>
-          <Typography paragraph>
-            Consequat mauris nunc congue nisi vitae suscipit. Fringilla est
-            ullamcorper eget nulla facilisi etiam dignissim diam. Pulvinar
-            elementum integer enim neque volutpat ac tincidunt. Ornare
-            suspendisse sed nisi lacus sed viverra tellus. Purus sit amet
-            volutpat consequat mauris. Elementum eu facilisis sed odio morbi.
-            Euismod lacinia at quis risus sed vulputate odio. Morbi tincidunt
-            ornare massa eget egestas purus viverra accumsan in. In hendrerit
-            gravida rutrum quisque non tellus orci ac. Pellentesque nec nam
-            aliquam sem et tortor. Habitant morbi tristique senectus et.
-            Adipiscing elit duis tristique sollicitudin nibh sit. Ornare aenean
-            euismod elementum nisi quis eleifend. Commodo viverra maecenas
-            accumsan lacus vel facilisis. Nulla posuere sollicitudin aliquam
-            ultrices sagittis orci a.
-          </Typography>
+          <Stack
+            sx={{ flexDirection: { xs: "column", md: "row" }, height: "88vh" }}
+          >
+            <Stack sx={{ width: { xs: "90vw", md: "70vw" }, height: "100%" }}>
+              <iframe
+                width="100%"
+                height="100%"
+                src="https://www.youtube.com/embed/U7V5rRYafCw"
+                title="YouTube video player"
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+              ></iframe>
+            </Stack>
+            <Stack
+              sx={{
+                pl: { xs: 0, md: 10 },
+                maxHeight: "100%",
+                overflow: "auto",
+                " &::-webkit-scrollbar": {
+                  width: "3px",
+                  height: "10px",
+                  bgcolor: "black",
+                  cursor: "pointer",
+                },
+                " &::-webkit-scrollbar-thumb": {
+                  background: "silver",
+                  borderRadius: "6px",
+                  width: "6px",
+                  cursor: "pointer",
+                },
+                pt: { xs: 2, md: 0 },
+              }}
+            >
+              <Typography paragraph>Có những tháng ngày</Typography>
+              <Typography paragraph>chỉ anh cùng đau buồn</Typography>
+              <Typography paragraph>
+                hoà vào cơn mưa chiều thu héo úa
+              </Typography>
+              <Typography paragraph>viết lên khúc nhạc</Typography>
+              <Typography paragraph>buồn tan tình tan tình</Typography>
+              <Typography paragraph>
+                vội lau nhẹ đi khoé mi sầu cho mình
+              </Typography>
+              <br />
+              <Typography paragraph>Chắc ta quá vội</Typography>
+              <Typography paragraph>trót trao tình duyên rồi</Typography>
+              <Typography paragraph>
+                giờ người xem ta giống như đùa vui thôi
+              </Typography>
+              <Typography paragraph>mắt em hững hờ</Typography>
+              <Typography paragraph>có ai nào đâu ngờ</Typography>
+              <Typography paragraph>1 ngày 2 ta cách xa</Typography>
+              <Typography paragraph>còn ai chờ</Typography>
+              <Typography paragraph></Typography>
+              <Typography paragraph>Và anh biết duyên mình đã lỡ</Typography>
+              <Typography paragraph>phận bọt bèo đâu dám mơ</Typography>
+              <Typography paragraph>trèo cao té đau</Typography>
+              <Typography paragraph>đành lui về sau</Typography>
+              <Typography paragraph>Biết mai này ta mất nhau</Typography>
+              <br />
+              <Typography paragraph>
+                Phận duyên ta ngắn đành thôi từ đây
+              </Typography>
+              <Typography paragraph>một mình anh lê bước về</Typography>
+              <Typography paragraph>câu hứa câu thề</Typography>
+              <Typography paragraph>mãi chôn vùi trong cơn mê</Typography>
+              <Typography paragraph>Nhìn em sanh đôi cùng duyên mới</Typography>
+              <Typography paragraph>lòng anh đau thắt nghẹn lời</Typography>
+              <Typography paragraph>
+                dẫu biết không thuộc về nhau nhưng sao lòng còn nhớ
+              </Typography>
+              <br />
+              <Typography paragraph>em về nới chốn khuê phòng </Typography>
+              <Typography paragraph>anh về nước mắt tuôn dòng</Typography>
+              <Typography paragraph>
+                chúc phúc cho em cùng người đến sau như đợi mong{" "}
+              </Typography>
+              <br />
+              <Typography paragraph>
+                dẫu biết là kết thúc chẳng níu kéo sao vẫn đợi
+              </Typography>
+              <Typography paragraph>
+                xoá hết từng kí ức từng nỗi nhớ đau thấu trời{" "}
+              </Typography>
+              <Typography paragraph>
+                anh sẽ chẳng khóc nữa chẳng yếu đuối đâu hỡi người
+              </Typography>
+              <Typography paragraph>
+                chúc em bình yên thôi bước đi đi môi cười tươi
+              </Typography>
+              <br />
+              <Typography paragraph>mãi còn đó </Typography>
+              <Typography paragraph>tình anh dành cho </Typography>
+              <Typography paragraph>
+                ngồi đợi trông ngày hình bóng e sang đò
+              </Typography>
+              <br />
+              <Typography paragraph>pháo hồng cưới</Typography>
+              <Typography paragraph>nhìn em cười tươi</Typography>
+              <Typography paragraph>
+                lệ trong tim trào dâng lòng anh đau ngàn đau
+              </Typography>
+              <br />
+            </Stack>
+          </Stack>
         </Main>
       </Box>
     </>
